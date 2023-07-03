07e28bf7b6835a97650d030d9a2486763d74e5c4
Kerriea-star <ojared511@gmail.com>
2023-07-03 14:31:08 +0300

    templates tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
