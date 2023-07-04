f99a26a9838d08653b542ba7b95253abfb65f92d
Kerriea-star <ojared511@gmail.com>
2023-07-04 09:53:14 +0300

    auth app

--- django_project/urls.py
+++ django_project/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+    # User management
+    path("accounts/", include("diango.contrib.auth.urls")),
+    # local apps
     path("", include("pages.urls")),
 ]
