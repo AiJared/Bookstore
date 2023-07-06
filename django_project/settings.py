e3f7e4c1fd53bfb83f39c5c4a2dbd6f2c4a250e6
Kerriea-star <ojared511@gmail.com>
2023-07-06 12:36:32 +0300

    environment virable

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,12 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*^bt+i(%f9rcedn7vvqk^zf5du7c*g$+qlgjjf47=dt+2)s&n4'
+SECRET_KEY = env("DJANGO_SECTRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
