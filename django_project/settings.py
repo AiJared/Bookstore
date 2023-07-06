f3f25ecbcb37a1287ddce1f958038b838ca6a80a
Kerriea-star <ojared511@gmail.com>
2023-07-06 09:55:14 +0300

    allauth

--- django_project/settings.py
+++ django_project/settings.py
@@ -142,3 +142,4 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend",
 )
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+ACCOUNT_SESSION_REMEMBER = True
